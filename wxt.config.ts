3b8b713d1d5a81887c66c6c49d7b8b31cc33f874
imjac0b <186802484+imjac0b@users.noreply.github.com>
2025-06-03 02:38:59 +0800

    add tailwind

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "wxt";
+import tailwindcss from "@tailwindcss/vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
@@ -6,5 +7,11 @@ export default defineConfig({
   manifest: {
     name: "Archer",
     permissions: ["sidePanel"],
+  },
+  vite(env) {
+    return {
+      ...env,
+      plugins: [tailwindcss()],
+    };
   },
 });
